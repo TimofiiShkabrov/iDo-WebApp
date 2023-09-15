89d1049fd0c42e9db293ca4af5f4b84f2e382437
Timofii Shkabrov <timofii.shkabrov@gmail.com>
2023-09-15 13:53:16 +0200

    Version: 1.2 -Visual edit
    Changes to be committed:
    	modified:   src/components/home/Home.jsx
    	modified:   src/components/home/home.css
    	modified:   src/styles/common.css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,25 +5,27 @@ const Home = () => {
     return ( 
         <section className="home">
             <div className="container">
-                <div className="home_content">
-                <div className="home_text">
-                    <div className="header_title">
-                    Turn your ideas into action!
-                    </div>
-                    <div className="header_description">
-                    Welcome to iDo - your trusted partner in everyday organisation 
-                    and goal achievement! iDo is a modern task management app 
-                    that will help you plan your day with ease, be more productive 
-                    and achieve your goals with confidence.
-                    <div className="home_btn-wrapper">
-                        <a href="#!" className="home_btn">
-                        SIGN IN
-                        </a>
-                    </div>
-                    </div>
+                <div className="home_container">
+                    <div className="home_content">
+                    <div className="home_text">
+                        <div className="header_title">
+                        Turn your ideas into action!
+                        </div>
+                        <div className="header_description">
+                        Welcome to iDo - your trusted partner in everyday organisation 
+                        and goal achievement! iDo is a modern task management app 
+                        that will help you plan your day with ease, be more productive 
+                        and achieve your goals with confidence.
+                        </div>
                     </div>
                     <div className="home_img">
-                    <img src={homeImg} alt="Phone" />
+                        <img src={homeImg} alt="Phone" />
+                    </div>
+                    </div>
+                    <div className="home_btn-wrapper">
+                        <a href="#!" className="home_btn-text">
+                        SIGN IN
+                        </a>
                     </div>
                 </div>
             </div>
